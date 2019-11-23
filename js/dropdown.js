2c2a822f75a5cc6a97a4d44f4b2f1b5cd0fbe57c
Cameron Varley <cam.avarley@gmail.com>
2019-11-23 15:53:45 -0500

    Feature Add: dropdown support, moved javascript to WIP

---
+++ js/dropdown.js
@@ -0,0 +1,23 @@
+var $dropdowns = getAll('.dropdown:not(.is-hoverable)');
+
+if ($dropdowns.length > 0) {
+    $dropdowns.forEach(function($el) {
+        $el.addEventListener('click', function(event) {
+            event.stopPropagation();
+            $el.classList.toggle('show');
+        });
+    });
+
+    document.addEventListener('click', function(event) {
+        closeDropdowns();
+    });
+}
+
+function closeDropdowns() {
+    $dropdowns.forEach(function($el) {
+        $el.classList.remove('show');
+    });
+}
+function getAll(selector) {
+    return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
+}
